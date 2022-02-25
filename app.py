5c7de4b92a056c6bedea9c3132f3cc68b364b928
Thienvu <thienvu.quach.simplon@gmail.com>
2022-02-25 18:47:20 +0100

    ok

--- app.py
+++ app.py
@@ -10,67 +10,75 @@ st.title('Job dashboard')
 lieu = pd.read_sql_query("""SELECT distinct(lieu) 
                             from evenement 
                             """, con)
-
 new_row = pd.DataFrame({'lieu':'Default'}, index =[0])
 new_lieu = pd.concat([new_row, lieu]).reset_index(drop = True)                           
 chosen_lieu = st.sidebar.selectbox('Lieu', new_lieu)
 
-job = ["Default","Data engineer","Data analyst","Data scientist"]
-chosen_job = st.sidebar.selectbox('Type de poste', job)
+job = pd.read_sql_query("""SELECT distinct categorie 
+                            from evenement 
+                            """, con)
+new_row_2 = pd.DataFrame({'categorie':'Default'}, index =[0])
+new_job = pd.concat([new_row_2, job]).reset_index(drop = True)
+chosen_job = st.sidebar.selectbox('Type de poste', new_job)
 
 langue = ["Français","Anglais"]
 chosen_langue = st.sidebar.selectbox('Langue', langue)
 
-if chosen_lieu != "Default":
-    if chosen_langue == "Français":
-        bdd_langue = "fr"
-        language_code = "fr-FR"
-    elif chosen_langue == "Anglais":
-        bdd_langue = "en"
-        language_code = "en-US"
+if chosen_langue == "Français":
+    bdd_langue = "fr"       
+elif chosen_langue == "Anglais":
+    bdd_langue = "en"
 
+if chosen_job != "Default" and chosen_lieu == "Default":
     df = pd.read_sql_query(f"""SELECT * 
-                            from evenement 
-                            WHERE lieu = "{chosen_lieu}" and langue = "{bdd_langue}";""", con)
+                        from evenement 
+                        WHERE langue = "{bdd_langue}" 
+                        and categorie = "{chosen_job}";""", con)
     st.dataframe(df.astype(str))
-
-    if len(df) != 0:
-        location = st.slider("Choissisez l'index de votre offre", -1,len(df)-1)
-        if location != -1:
-            test2 = ["Offre entière", "Mots clés"]
-            test = st.selectbox("Choissisez le mode", test2)
-        try:
-            description = df.iloc[location]["description"]
-            offre = df.iloc[location]["intitule_offre"]
-            lieu = df.iloc[location]["lieu"]
-            entreprise = df.iloc[location]["nom_entreprise"]
-            date = df.iloc[location]["date_publication_annonce"]
-            id = df.iloc[location]["id"]
-            key_words = df.iloc[location]["key_words"]
-            if test == "Offre entière":
-                st.write("Intitulé : ", offre)
-                st.write("Lieu : ", lieu)
-                st.write("Nom entreprise : ", entreprise)
-                st.write("Date de publication : ", date)
-                st.write(description)
-                #st.write("key words : ",key_words)
-            elif test == "Mots clés":
-                st.write("Key words : ")
-                st.write(key_words)
-               
-        except:
-            st.write("")
-    else :
-        st.write("Aucune offre disponible")
-
-if chosen_job == "Data analyst":
-    df = pd.read_sql_query("""select nom_commune, avg(valeur_fonciere/surface_reelle_bati) 
-                            from dvf 
-                            where type_local="Appartement" and code_departement = '38'
-                            group by nom_commune 
-                            order by avg(valeur_fonciere/surface_reelle_bati)  ASC
-                            limit 20;""", con)
+elif chosen_job == "Default" and chosen_lieu != "Default":
+    df = pd.read_sql_query(f"""SELECT * 
+                        from evenement 
+                        WHERE langue = "{bdd_langue}" 
+                        and lieu = "{chosen_lieu}";""", con)
     st.dataframe(df.astype(str))
+elif chosen_job != "Default" and chosen_lieu != "Default":
+    df = pd.read_sql_query(f"""SELECT * 
+                        from evenement 
+                        WHERE langue = "{bdd_langue}" 
+                        and lieu = "{chosen_lieu}"
+                        and categorie = "{chosen_job}";""", con)
+    st.dataframe(df.astype(str))
+else:
+    df=""
+
+if len(df) != 0:
+    location = st.slider("Choissisez l'index de votre offre", -1,len(df)-1)
+    if location != -1:
+        test2 = ["Offre entière", "Mots clés"]
+        test = st.selectbox("Choissisez le mode", test2)
+    try:
+        description = df.iloc[location]["description"]
+        offre = df.iloc[location]["intitule_offre"]
+        lieu = df.iloc[location]["lieu"]
+        entreprise = df.iloc[location]["nom_entreprise"]
+        date = df.iloc[location]["date_publication_annonce"]
+        id = df.iloc[location]["id"]
+        key_words = df.iloc[location]["key_words"]
+        if test == "Offre entière":
+            st.write("Intitulé : ", offre)
+            st.write("Lieu : ", lieu)
+            st.write("Nom entreprise : ", entreprise)
+            st.write("Date de publication : ", date)
+            st.write(description)
+            #st.write("key words : ",key_words)
+        elif test == "Mots clés":
+            st.write("Key words : ")
+            st.write(key_words)
+            
+    except:
+        st.write("")
+else :
+    st.write("Aucune offre disponible")
 
 con.close()
 
